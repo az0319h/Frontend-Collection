533b9514aa0ea06c74402b23df6a88ad96aec21f
Hoon.hs <49442904+az0319h@users.noreply.github.com>
2025-02-25 01:39:45 +0900

    울트라 코리아(25.02.24~작성중)

--- ULTRA KOREA/scripts/header.js
+++ ULTRA KOREA/scripts/header.js
@@ -1,12 +1,15 @@
 function scrollEvent(e) {
     const headerLeft = document.querySelector('.header_left')
     const headerRight = document.querySelector('.header_right')
+    const headerContainer = document.querySelector('.header_container')
     if(window.scrollY > 0) {
         headerLeft.classList.add('scrollAvtive')      
-        headerRight.classList.add('scrollAvtive')      
+        headerRight.classList.add('scrollAvtive')  
+        headerContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.9)'    
     } else {
         headerLeft.classList.remove('scrollAvtive')      
         headerRight.classList.remove('scrollAvtive')      
+        headerContainer.style.backgroundColor = 'rgba(0, 0, 0, 0)'
     }
 }
 
