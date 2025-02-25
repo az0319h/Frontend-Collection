9fed3030b2ff37cd41ed47d128bf05b7681ece7a
Hoon.hs <49442904+az0319h@users.noreply.github.com>
2025-02-25 18:43:51 +0900

    울트라 코리아(25.02.24~작성중)

--- ULTRA KOREA/scripts/header.js
+++ ULTRA KOREA/scripts/header.js
@@ -1,3 +1,26 @@
+const hamburgerMenuOpenBtn = document.querySelector('.hamburger_menu')
+const hamburgerMenuCloseBtn = document.querySelector(".menu_toggle_button")
+const mobileMenu = document.querySelector(".mobile_menu")
+const menulinks = document.querySelectorAll('.menu_link')
+ 
+menulinks.forEach((item, idx) => {
+    item.addEventListener('click', (e)=> {
+        if(item.parentNode.querySelector('.submenu') != null) {
+            e.preventDefault(); 
+            const subMenu = item.parentNode.querySelector('.submenu')
+            subMenu.style.display = subMenu.style.display == 'none' || subMenu.style.display == '' ? "block" : "none"
+            item.querySelector('.expand_icon').textContent = item.querySelector('.expand_icon').textContent === '+' ? "-" : "+"
+        }
+    })
+})
+
+hamburgerMenuOpenBtn.addEventListener('click',(e)=> {
+    mobileMenu.style.display = 'block'
+})
+hamburgerMenuCloseBtn.addEventListener('click', (e) => {
+    mobileMenu.style.display = 'none'
+})
+
 function scrollEvent(e) {
     const headerLeft = document.querySelector('.header_left')
     const headerRight = document.querySelector('.header_right')
@@ -14,3 +37,4 @@ function scrollEvent(e) {
 }
 
 window.addEventListener('scroll', scrollEvent)
+
